45896fe04a8b74a17378cd4f4ce37380ea362837
ador <rashedulhaque111@gmail.com>
2022-10-05 12:10:42 +0600

    create event hook form design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import CreateEvent from './Components/CreateEvent/CreateEvent';
+import Events from './Components/Events/Events';
 import LandingPage from './Components/Landing Page/LandingPage';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/create" element={<CreateEvent />} />
+        <Route path="/event" element={<Events />} />
       </Routes>
 
     </div>
--- src/Components/CreateEvent/CreateEvent.jsx
+++ src/Components/CreateEvent/CreateEvent.jsx
@@ -1,9 +1,72 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import "./CreateEvent.css";
 
 const CreateEvent = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+    const navigate = useNavigate();
+
+    const onSubmit = (data) => {
+        console.log("RESULT", data);
+    };
+
+    const handleNext = () => {
+        navigate("/event");
+    };
+
     return (
-        <div>
-            <h1>Hello</h1>
+        <div className="eventDetailsForm">
+            <h1 className="text-3xl m-4" style={{ color: "#240D57" }}>Create Your Event</h1>
+            <form className="eventForm" onSubmit={handleSubmit(onSubmit)}>
+                <label>Event name</label>
+                <input
+                    type="text"
+                    {...register("eventName", { required: true, maxLength: 80 })}
+                />
+
+                <label>Host name</label>
+                <input
+                    type="text"
+                    {...register("hostName", { required: true, maxLength: 100 })}
+                />
+
+                <label>Starting Time</label>
+                <input
+                    type="text"
+                    {...register("startingTime", {
+                        required: true,
+                    })}
+                />
+                <label>Ending Time</label>
+                <input
+                    type="text"
+                    {...register("endingTime", {
+                        required: true,
+                    })}
+                />
+
+                <label>Event Location</label>
+                <input
+                    type="text"
+                    {...register("eventLocation", { required: true, maxLength: 80 })}
+                />
+
+                <label>Event Photo</label>
+                <input type="text" {...register("eventPhoto", { required: true })} />
+
+                <br />
+                <input className="submit" type="submit" value="Submit" />
+                <div className="btn4">
+                    <button onClick={handleNext} className="nextBtn">
+                        Next
+                    </button>
+                </div>
+            </form>
         </div>
     );
 };
